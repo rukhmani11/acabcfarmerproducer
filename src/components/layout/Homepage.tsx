d4c090ec4fe5c9ee7991e8960e8827c0dfa610f5
rukhmani11 <prajapatirukmani922@gmail.com>
2025-04-21 00:02:04 +0530

    create project

---
+++ src/components/layout/Homepage.tsx
@@ -0,0 +1,321 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Grid,
+  TextField,
+  CardActions,
+  CardHeader,
+  Card,
+  CardContent,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Container,
+  CssBaseline,
+  Paper,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+  Stack,
+  Switch,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+
+const Homepage = (...props: any) => {
+
+  //const { societyBuildingId, societyBuildingUnitId }: any = useParams();
+  const [title, setTitle] = useState<any>({});
+//   const { goToHome } = useSharedNavigation();
+  let navigate = useNavigate();
+  //const [UnitType, setUnitType] = useState<SelectListModel[]>([]);
+
+
+//   const validate = (fieldValues: SocietyBuildingUnitsModel = values) => {
+//     let temp: any = { ...errors };
+//     if ("Unit" in fieldValues)
+//       temp.Unit = fieldValues.Unit ? "" : "Unit is required.";
+//     if ("UnitTypeId" in fieldValues)
+//       temp.UnitTypeId = fieldValues.UnitTypeId ? "" : "UnitType is required.";
+//     if ("FloorNo" in fieldValues)
+//       temp.FloorNo = fieldValues.FloorNo >= 0 ? "" : "FloorNo is required.";
+//     if ("TerraceArea" in fieldValues)
+//       temp.TerraceArea = fieldValues.TerraceArea ? (temp.TerraceArea = /^[0-9]*(\.[0-9]{0,3})?$/.test(fieldValues.TerraceArea.toString()) ? "" : "") : ""
+   
+//     setErrors({
+//       ...temp,
+//     });
+
+//     if (fieldValues === values)
+//       return Object.values(temp).every((x) => x === "");
+//   };
+
+//   const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+//     useForm(
+//       societyBuildingUnitsService.initialFieldValues,
+//       validate,
+//       props.SocietyBuildingUnitId
+//     );
+
+//   const newsocietyBuildingUnits = () => {
+//     setValues(societyBuildingUnitsService.initialFieldValues);
+//   };
+
+//   function setFormValue(model: SocietyBuildingUnitsModel) {
+//     let newModel = {
+//       SocietyBuildingUnitId: model.SocietyBuildingUnitId,
+//       SocietyBuildingId: model.SocietyBuildingId,
+//       Unit: model.Unit,
+//       UnitTypeId: model.UnitTypeId,
+     
+//     };
+//     return newModel;
+//   }
+
+
+
+
+ 
+  useEffect(() => {
+    // if (societyBuildingUnitId) {
+    //   getSocietyBuildingUnit(societyBuildingUnitId);
+    // } else {
+    //   newsocietyBuildingUnits();
+    //   setErrors({});
+    // }
+  }, []);
+
+ 
+  return (
+    <>
+      <Stack direction="row" spacing={0} justifyContent="space-between">
+        <Typography variant="h5" align="center">
+          Building Units
+        </Typography>
+        <Typography variant="body1"><b>Building : </b>{title.Building} </Typography>
+      </Stack>
+
+      <form autoComplete="off" noValidate 
+      >
+        <Card>
+          <CardHeader title="Building Unit" />
+          <CardContent>
+            <React.Fragment>
+              <Grid container spacing={5}>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    required
+                    label="Unit"
+                    name="Unit"
+                    value={values.Unit}
+                    onChange={handleInputChange}
+                    error={errors.Unit}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Select
+                    showEmptyItem={false}
+                    name="UnitTypeId"
+                    label="UnitType"
+                    required
+                    value={UnitType.length > 0 ? values.UnitTypeId : ""}
+                    onChange={handleInputChange}
+                    options={UnitType}
+                    error={errors.UnitTypeId}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    required
+                    label="Floor No"
+                    name="FloorNo"
+                    type="number"
+                    value={values.FloorNo}
+                    onChange={handleInputChange}
+                    error={errors.FloorNo}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="Wing"
+                    name="Wing"
+                    value={values.Wing}
+                    onChange={handleInputChange}
+                    error={errors.Wing}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="CarpetArea"
+                    name="CarpetArea"
+                    type="number"
+                    value={values.CarpetArea}
+                    onChange={handleInputChange}
+                    error={errors.CarpetArea}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="ChargeableArea"
+                    name="ChargeableArea"
+                    type="number"
+                    value={values.ChargeableArea}
+                    onChange={handleInputChange}
+                    error={errors.ChargeableArea}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="TerraceArea"
+                    name="TerraceArea"
+                    type="number"
+                    value={values.TerraceArea}
+                    onChange={handleInputChange}
+                    error={errors.TerraceArea}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.ReactDatePicker
+                    label="StartDate"
+                    name="StartDate"
+                    value={values.StartDate}
+                    onChange={(date: Date) => handleInputChange({ target: { value: globalService.convertLocalToUTCDate(date), name: 'StartDate' } })}
+                    error={errors.StartDate}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.ReactDatePicker
+                    label="EndDate"
+                    name="EndDate"
+                    value={values.EndDate}
+                    onChange={(date: Date) => handleInputChange({ target: { value: globalService.convertLocalToUTCDate(date), name: 'EndDate' } })}
+                    error={errors.EndDate}
+                  />
+                </Grid>
+              </Grid>
+            </React.Fragment>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader title="Share Certificate Details" />
+          <CardContent>
+            <React.Fragment>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="CertificateNo"
+                    type="number"
+                    name="CertificateNo"
+                    value={values.CertificateNo}
+                    onChange={handleInputChange}
+                    error={errors.CertificateNo}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="NoOfShares"
+                    type="number"
+                    name="NoOfShares"
+                    value={values.NoOfShares}
+                    onChange={handleInputChange}
+                    error={errors.NoOfShares}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="DistinctiveFrom"
+                    name="DistinctiveFrom"
+                    type="number"
+                    value={values.DistinctiveFrom}
+                    onChange={handleInputChange}
+                    error={errors.DistinctiveFrom}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="DistinctiveTo"
+                    name="DistinctiveTo"
+                    type="number"
+                    value={values.DistinctiveTo}
+                    onChange={handleInputChange}
+                    error={errors.DistinctiveTo}
+                  />
+                </Grid>
+                {/* <text >Value</text> */}
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="Value"
+                    name="Value"
+                    type="number"
+                    value={values.Value}
+                    onChange={handleInputChange}
+                    error={errors.Value}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.Input
+                    label="AmountAtAllotment"
+                    name="AmountAtAllotment"
+                    type="number"
+                    value={values.AmountAtAllotment}
+                    onChange={handleInputChange}
+                    error={errors.AmountAtAllotment}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.ReactDatePicker
+                    label="PayDate"
+                    name="PayDate"
+                    value={values.PayDate}
+                    onChange={(date: Date) => handleInputChange({ target: { value: globalService.convertLocalToUTCDate(date), name: 'PayDate' } })}
+                    error={errors.PayDate}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Controls.ReactDatePicker
+                    fullWidth
+                    label="IssueDate"
+                    name="IssueDate"
+                    value={values.IssueDate}
+                    onChange={(date: Date) => handleInputChange({ target: { value: globalService.convertLocalToUTCDate(date), name: 'IssueDate' } })}
+                    error={errors.IssueDate}
+                  />
+                </Grid>
+
+              </Grid>
+            </React.Fragment>
+          </CardContent>
+        </Card>
+
+        <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+          <Stack spacing={2} direction="row">
+            <Button type="submit" variant="contained" className={!globalService.roleMatch([ROLES.ReadOnly], auth) ? '' : 'hidden'}
+            >
+              Submit
+            </Button>
+            <Button
+              variant="outlined"
+              startIcon={<ArrowBackIcon />}
+              // onClick={() =>
+              //   navigate("/societyBuildingUnits/" + societyBuildingId) }
+              onClick={() => navigate(-1)}
+
+            >
+              Back To List
+            </Button>
+          </Stack>
+        </CardActions>
+      </form>
+    </>
+  );
+};
+
+export default Homepage;
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,152 @@
+import { alpha, Button, Link, Menu, MenuProps, styled, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import MenuItem from "@mui/material/MenuItem";
+const StyledMenu = styled((props: MenuProps) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    "& .MuiPaper-root": {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === "light"
+          ? "rgb(55, 65, 81)"
+          : theme.palette.grey[300],
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      "& .MuiMenu-list": {
+        padding: "4px 0",
+      },
+      "& .MuiMenuItem-root": {
+        "& .MuiSvgIcon-root": {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        "&:active": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity
+          ),
+        },
+      },
+    },
+  }));
+  
+const Navbar: React.FC = () =>  {
+    const handleClick = (
+        event: React.MouseEvent<HTMLElement>,
+        mainMenu: string
+      ) => {
+        if (mainMenu === "common") setCommonAnchorEl(event.currentTarget);
+        // else if (mainMenu === "adminSociety")
+        //   setAdminSocietyAnchorEl(event.currentTarget);
+      
+      };
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+  
+      const [commonAnchorEl, setCommonAnchorEl] =
+          React.useState<null | HTMLElement>(null);
+
+          const openCommon = Boolean(commonAnchorEl);
+       const handleClose = () => {
+          setCommonAnchorEl(null);
+         // setAdminSocietyAnchorEl(null);
+        
+        };
+    
+  return (
+    <div>
+       <>
+      <React.Fragment>
+      <Toolbar
+  component="nav"
+  variant="dense"
+  sx={{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "var(--primary-color)",
+    color: "white",
+    textDecoration: "none",
+    overflowX: "auto",
+    paddingInline: 2,
+  }}
+>
+  {/* Left-aligned Typography */}
+  <Typography
+  sx={{
+    fontSize: "1.2rem",
+    fontWeight: 500,
+    fontStyle: "italic",
+    color: "white",
+    letterSpacing: "0.03em",
+  }}
+>
+ACABC FARMER PRODUCER COMPANY LIMITED
+</Typography>
+
+
+
+  {/* Right-aligned Common button */}
+  <Button
+    id="common-button"
+    aria-controls={open ? "common-menu" : undefined}
+    aria-haspopup="true"
+    aria-expanded={open ? "true" : undefined}
+    sx={{
+      color: "white",
+      '& .MuiSvgIcon-root': {
+        color: 'white',
+      },
+    }}
+    disableElevation
+    onClick={(e) => handleClick(e, "common")}
+    endIcon={<KeyboardArrowDownIcon />}
+  >
+    Common
+  </Button>
+
+  {/* Menu */}
+  <StyledMenu
+    id="common-menu"
+    MenuListProps={{
+      "aria-labelledby": "common-button",
+    }}
+    anchorEl={commonAnchorEl}
+    open={openCommon}
+    onClose={handleClose}
+  >
+    <Link color="inherit" href={"/banks"} className="nav-link">
+      <MenuItem onClick={handleClose} disableRipple>
+        Bank
+      </MenuItem>
+    </Link>
+    <Link color="inherit" href={"/parkingTypes"} className="nav-link">
+      <MenuItem onClick={handleClose} disableRipple>
+        Parking Type
+      </MenuItem>
+    </Link>
+  </StyledMenu>
+</Toolbar>
+
+      </React.Fragment>
+    </>
+    </div>
+  )
+}
+
+export default Navbar;
